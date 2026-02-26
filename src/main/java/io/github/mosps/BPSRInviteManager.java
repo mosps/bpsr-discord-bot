b89afeeef6f82df4347f118c37afdef9dcc559a0
mosps <Mos.ps2020@gmail.com>
2026-02-27 03:59:29 +0900

    botStart()を実装

--- src/main/java/io/github/mosps/BPSRInviteManager.java
+++ src/main/java/io/github/mosps/BPSRInviteManager.java
@@ -1,19 +1,22 @@
 package io.github.mosps;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class BPSRInviteManager {
 
     public void botStart(String[] args) {
         String token = args[0];
 
-        try {
-             jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
-                    .addEventListeners(
-                            ImagineChecker(),
-                            CommandListener()
-                    )
-                    .setActivity(Activity.playing("/bic"))
-                    .setRawEventsEnabled(true)
-                    .build()
-        }
+        JDA jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
+               .addEventListeners(
+
+               )
+               .setActivity(Activity.playing("デバッグ中"))
+               .setRawEventsEnabled(true)
+               .build();
+        jda.upsertCommand("p", "テスト").queue();
     }
 }
