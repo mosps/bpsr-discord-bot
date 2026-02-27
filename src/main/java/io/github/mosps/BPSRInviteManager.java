42251fc869cae4caabc628a7df3ea35300740aa1
mosps <Mos.ps2020@gmail.com>
2026-02-28 00:15:55 +0900

    ListenerクラスをJDABuilderに追加

--- src/main/java/io/github/mosps/BPSRInviteManager.java
+++ src/main/java/io/github/mosps/BPSRInviteManager.java
@@ -1,5 +1,6 @@
 package io.github.mosps;
 
+import io.github.mosps.listeners.SlashCommandListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -12,7 +13,7 @@ public class BPSRInviteManager {
 
         JDA jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
                .addEventListeners(
-
+                       new SlashCommandListener()
                )
                .setActivity(Activity.playing("デバッグ中"))
                .setRawEventsEnabled(true)
