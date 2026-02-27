44325cb48d8827c164b80c8e65f9decf4af1c3f1
mosps <Mos.ps2020@gmail.com>
2026-02-28 00:11:56 +0900

    startCleaner()の実行を追加

--- src/main/java/io/github/mosps/Main.java
+++ src/main/java/io/github/mosps/Main.java
@@ -1,7 +1,10 @@
 package io.github.mosps;
 
+import io.github.mosps.session.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
         new BPSRInviteManager().botStart(args);
+        SessionManager.startCleaner();
     }
 }
