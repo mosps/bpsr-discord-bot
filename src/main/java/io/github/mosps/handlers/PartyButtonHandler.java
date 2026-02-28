5c7d4c542a26bafdb1bcb202653493f3ef4b83e1
mosps <Mos.ps2020@gmail.com>
2026-02-28 20:44:48 +0900

    handle()の処理を追加

--- src/main/java/io/github/mosps/handlers/PartyButtonHandler.java
+++ src/main/java/io/github/mosps/handlers/PartyButtonHandler.java
@@ -7,4 +7,45 @@ import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
 public class PartyButtonHandler implements ButtonHandler {
 
+    public boolean handle(ButtonInteractionEvent event) {
+        if (!event.getComponentId().startsWith("party")) return false;
+
+        long messageId = event.getMessageIdLong();
+        SessionData session = SessionManager.getSession(messageId);
+
+        if (session == null) {
+            event.reply("期限切れの募集です。").setEphemeral(true).queue();
+            return true;
+        }
+        if (session.isClosed()) {
+            event.reply("締め切られた募集です。").setEphemeral(true).queue();
+            return true;
+        }
+
+        long userId = event.getUser().getIdLong();
+
+        switch (event.getComponentId()) {
+            case "party_join":
+                session.addMembers(userId);
+                break;
+            case "party_leave":
+                session.removeMembers(userId);
+                break;
+            case "party_close":
+                if (userId != session.getOwnerId()) {
+                    event.reply("募集作成者ではありません").setEphemeral(true).queue();
+                    return true;
+                }
+                SessionManager.removeSession(messageId);
+                break;
+        }
+
+        event.editMessageEmbeds(
+                MessageRenderer.render(session)
+        ).setComponents(
+                MessageRenderer.buttons(session)
+        ).queue();
+
+        return true;
+    }
 }
