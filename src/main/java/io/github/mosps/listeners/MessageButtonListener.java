711a30a2b8084f0d394d4cd52c93798031db9dd0
mosps <Mos.ps2020@gmail.com>
2026-02-28 20:41:42 +0900

    Handlerインターフェースを用いた処理分岐を実装

--- src/main/java/io/github/mosps/listeners/MessageButtonListener.java
+++ src/main/java/io/github/mosps/listeners/MessageButtonListener.java
@@ -1,6 +1,25 @@
 package io.github.mosps.listeners;
 
+import io.github.mosps.handlers.ButtonHandler;
+import io.github.mosps.handlers.PartyButtonHandler;
+import io.github.mosps.session.SessionData;
+import io.github.mosps.session.SessionManager;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessageButtonListener extends ListenerAdapter {
+    private final List<ButtonHandler> handlers = List.of(
+            new PartyButtonHandler()
+    );
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+        for (ButtonHandler handler : handlers) {
+            if (handler.handle(event)) return;
+        }
+    }
 }
