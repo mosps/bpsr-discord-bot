a095a36c214f36ff0c9c15c39c4b21ce4c1866cc
mosps <Mos.ps2020@gmail.com>
2026-02-28 21:21:54 +0900

    クラスを追加

---
+++ src/main/java/io/github/mosps/handlers/ButtonHandlerManager.java
@@ -0,0 +1,11 @@
+package io.github.mosps.handlers;
+
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ButtonHandlerManager {
+
+
+}
