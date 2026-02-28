5db4be0c7bf18b9dd088d1a8ab29c5f011ccc4f1
mosps <Mos.ps2020@gmail.com>
2026-02-28 21:20:24 +0900

    ButtonHandlerManagerに処理分岐処理を移行

--- src/main/java/io/github/mosps/listeners/MessageButtonListener.java
+++ src/main/java/io/github/mosps/listeners/MessageButtonListener.java
@@ -1,6 +1,7 @@
 package io.github.mosps.listeners;
 
 import io.github.mosps.handlers.ButtonHandler;
+import io.github.mosps.handlers.ButtonHandlerManager;
 import io.github.mosps.handlers.PartyButtonHandler;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -9,14 +10,9 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class MessageButtonListener extends ListenerAdapter {
-    private final List<ButtonHandler> handlers = List.of(
-            new PartyButtonHandler()
-    );
 
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
-        for (ButtonHandler handler : handlers) {
-            if (handler.handle(event)) return;
-        }
+        ButtonHandlerManager.handle(event);
     }
 }
