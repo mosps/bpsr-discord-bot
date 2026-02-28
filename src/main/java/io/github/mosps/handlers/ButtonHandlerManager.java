8565f03087cde55fcb8844218811d35924e82896
mosps <Mos.ps2020@gmail.com>
2026-02-28 21:27:05 +0900

    ButtonのComponentIdを用いた処理分岐,handlerへの値渡しを実装

--- src/main/java/io/github/mosps/handlers/ButtonHandlerManager.java
+++ src/main/java/io/github/mosps/handlers/ButtonHandlerManager.java
@@ -7,5 +7,23 @@ import java.util.Map;
 
 public class ButtonHandlerManager {
 
+    private static final Map<String, ButtonHandler> handlers = new HashMap<>();
 
+    static {
+        register("party", new PartyButtonHandler());
+    }
+
+    public static void register(String prefix, ButtonHandler handler) {
+        handlers.put(prefix, handler);
+    }
+
+    public static void handle(ButtonInteractionEvent event) {
+        String[] parts = event.getComponentId().split(":");
+
+        ButtonHandler handler = handlers.get(parts[0]);
+
+        if (handler != null) {
+            handler.handle(event, parts);
+        }
+    }
 }
