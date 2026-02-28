0c8d599222bde786dcf853828c9bcb3f40a889b8
mosps <Mos.ps2020@gmail.com>
2026-02-28 20:39:54 +0900

    クラスを追加

---
+++ src/main/java/io/github/mosps/handlers/PartyButtonHandler.java
@@ -0,0 +1,10 @@
+package io.github.mosps.handlers;
+
+import io.github.mosps.render.MessageRenderer;
+import io.github.mosps.session.SessionData;
+import io.github.mosps.session.SessionManager;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+
+public class PartyButtonHandler implements ButtonHandler {
+
+}
