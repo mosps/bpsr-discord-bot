0220d643bdd3323031927ee383d1961861797fef
mosps <Mos.ps2020@gmail.com>
2026-03-03 02:39:53 +0900

    クラスを追加

---
+++ src/main/java/io/github/mosps/render/RendererRegistry.java
@@ -0,0 +1,11 @@
+package io.github.mosps.render;
+
+import io.github.mosps.render.messages.PartyMessage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RendererRegistry {
+
+
+}
---
+++ src/main/java/io/github/mosps/render/messages/PartyMessage.java
@@ -0,0 +1,9 @@
+package io.github.mosps.render.messages;
+
+import io.github.mosps.render.Renderer;
+import net.dv8tion.jda.api.utils.messages.MessageEditData;
+
+public class PartyMessage implements Renderer<PartyMessage> {
+
+
+}
