6031a2e1f63e6d952009834ed98931e632238988
mosps <Mos.ps2020@gmail.com>
2026-03-03 03:10:50 +0900

    クラスを追加

---
+++ src/main/java/io/github/mosps/render/RenderResult.java
@@ -0,0 +1,10 @@
+package io.github.mosps.render;
+
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.util.List;
+
+public class RenderResult {
+
+}
