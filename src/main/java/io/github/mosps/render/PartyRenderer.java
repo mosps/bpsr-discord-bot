795c99d11bb2f3dfb5d4cd425979343187e31e0b
mosps <Mos.ps2020@gmail.com>
2026-03-03 03:38:51 +0900

    不要なクラスを削除

--- src/main/java/io/github/mosps/render/PartyRenderer.java
+++
@@ -1,4 +0,0 @@
-package io.github.mosps.render;
-
-public class PartyRenderer {
-}
--- src/main/java/io/github/mosps/render/messages/PartyMessage.java
+++
@@ -1,9 +0,0 @@
-package io.github.mosps.render.messages;
-
-import io.github.mosps.render.Renderer;
-import net.dv8tion.jda.api.utils.messages.MessageEditData;
-
-public class PartyMessage implements Renderer<PartyMessage> {
-
-
-}
