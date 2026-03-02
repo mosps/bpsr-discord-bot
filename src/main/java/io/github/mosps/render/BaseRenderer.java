fb2e6d72e6578b5cb132a83e0c460eee6597b1e3
mosps <Mos.ps2020@gmail.com>
2026-03-03 03:41:20 +0900

    基本的な描画処理を記述

--- src/main/java/io/github/mosps/render/BaseRenderer.java
+++ src/main/java/io/github/mosps/render/BaseRenderer.java
@@ -1,4 +1,20 @@
 package io.github.mosps.render;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 public abstract class BaseRenderer<T> implements Renderer<T> {
+
+    protected EmbedBuilder baseEmbed() {
+        return new EmbedBuilder()
+                .setColor(Color.CYAN);
+    }
+
+    protected RenderResult build(EmbedBuilder embed, ActionRow rows) {
+        return new RenderResult(embed.build(), List.of(rows));
+    }
 }
