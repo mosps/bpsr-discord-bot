e337359f49db6e7b9595a66d2f17cdd1cba7d792
mosps <Mos.ps2020@gmail.com>
2026-03-03 03:42:43 +0900

    描画結果保存用fieldを作成

--- src/main/java/io/github/mosps/render/RenderResult.java
+++ src/main/java/io/github/mosps/render/RenderResult.java
@@ -6,5 +6,19 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import java.util.List;
 
 public class RenderResult {
+    private final MessageEmbed embed;
+    private final List<ActionRow> components;
 
+    public RenderResult(MessageEmbed embed, List<ActionRow> components) {
+        this.embed = embed;
+        this.components = components;
+    }
+
+    public MessageEmbed getEmbed() {
+        return embed;
+    }
+
+    public List<ActionRow> getComponents() {
+        return components;
+    }
 }
