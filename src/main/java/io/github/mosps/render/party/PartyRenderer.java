6cb939e8e0f889135e1b95144da6c8891bffc716
mosps <Mos.ps2020@gmail.com>
2026-03-03 03:41:00 +0900

    再度クラスを作成. PartyMessage描画用の処理内容を記述

---
+++ src/main/java/io/github/mosps/render/party/PartyRenderer.java
@@ -0,0 +1,36 @@
+package io.github.mosps.render.party;
+
+import io.github.mosps.render.BaseRenderer;
+import io.github.mosps.render.RenderResult;
+import io.github.mosps.views.party.PartyView;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+import net.dv8tion.jda.api.components.buttons.Button;
+
+public class PartyRenderer extends BaseRenderer<PartyView> {
+
+    @Override
+    public RenderResult render(PartyView view) {
+        EmbedBuilder embedBuilder = baseEmbed();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        view.members.forEach(member ->
+                stringBuilder.append("⊳").append(member).append('\n')
+        );
+
+        embedBuilder.addField("参加者",
+                stringBuilder.isEmpty() ? " " : stringBuilder.toString(),
+                false
+        );
+
+        embedBuilder.setFooter(
+                view.closed ? "募集終了" : "参加受付中"
+        );
+
+        Button join = Button.success("party:join:" + view.sessionId, "参加");
+        Button leave = Button.success("party:leave:" + view.sessionId, "退出");
+        Button close = Button.success("party:close:" + view.sessionId, "終了");
+
+        return build(embedBuilder, ActionRow.of(join, leave, close));
+    }
+}
