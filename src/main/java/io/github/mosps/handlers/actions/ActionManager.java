ab92d780defb9e1abc82392cffedc2c967ea4851
mosps <Mos.ps2020@gmail.com>
2026-03-04 21:00:46 +0900

    クラスを追加

---
+++ src/main/java/io/github/mosps/handlers/actions/ActionManager.java
@@ -0,0 +1,14 @@
+package io.github.mosps.handlers.actions;
+
+import io.github.mosps.handlers.actions.party.PartyCloseAction;
+import io.github.mosps.handlers.actions.party.PartyCreateAction;
+import io.github.mosps.handlers.actions.party.PartyJoinAction;
+import io.github.mosps.handlers.actions.party.PartyLeaveAction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActionManager {
+
+
+}
---
+++ src/main/java/io/github/mosps/handlers/handler/CommandHandler.java
@@ -0,0 +1,10 @@
+package io.github.mosps.handlers.handler;
+
+import io.github.mosps.handlers.actions.ActionContext;
+import io.github.mosps.handlers.actions.ActionManager;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class CommandHandler {
+
+
+}
