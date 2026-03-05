68ada76b6e4008f629ade9981ac12b93e8f805bf
mosps <Mos.ps2020@gmail.com>
2026-03-05 22:25:32 +0900

    Partyのデザインを変更

--- src/main/java/io/github/mosps/render/party/PartyRenderer.java
+++ src/main/java/io/github/mosps/render/party/PartyRenderer.java
@@ -4,6 +4,7 @@ import io.github.mosps.render.BaseRenderer;
 import io.github.mosps.render.RenderResult;
 import io.github.mosps.views.party.PartyView;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.components.actionrow.ActionRow;
 import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.utils.messages.MessageEditData;
@@ -16,21 +17,31 @@ public class PartyRenderer extends BaseRenderer<PartyView> {
 
         StringBuilder stringBuilder = new StringBuilder();
         view.members.forEach(member ->
-                stringBuilder.append("⊳").append(member).append('\n')
+                stringBuilder.append("<@").append(member).append(">\n")
         );
 
-        embedBuilder.addField("参加者",
-                stringBuilder.isEmpty() ? " " : stringBuilder.toString(),
+        embedBuilder.setTitle("パーティ募集");
+
+        embedBuilder.setDescription(
+                        "-----------------------------------\n" +
+                        "目的地: カナミア\n" +
+                        "-----------------------------------"
+                        );
+
+        embedBuilder.addField("参加者 " + "```" + view.members.size() + "/" + view.maxMembers + "```",
+                stringBuilder.isEmpty() ? "　" : stringBuilder.toString(),
                 false
-        );
-
-        embedBuilder.setFooter(
-                view.closed ? "募集終了" : "参加受付中"
         );
 
         Button join = Button.success("party:join:" + view.partyId, "参加");
         Button leave = Button.danger("party:leave:" + view.partyId, "退出");
-        Button close = Button.primary("party:close:" + view.partyId, "終了");
+        Button close = Button.secondary("party:close:" + view.partyId, "終了");
+
+        if (view.closed) {
+            join = join.asDisabled();
+            leave = leave.asDisabled();
+            close = close.asDisabled();
+        }
 
         return build(MessageEditData.fromEmbeds(embedBuilder.build()), ActionRow.of(join, leave, close));
     }
