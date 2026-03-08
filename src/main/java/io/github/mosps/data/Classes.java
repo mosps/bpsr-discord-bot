e304d6ce19bcfc27c665da0af807a5b8c4e5e1a7
mosps <Mos.ps2020@gmail.com>
2026-03-09 01:31:02 +0900

    Emojiを追加

--- src/main/java/io/github/mosps/data/Classes.java
+++ src/main/java/io/github/mosps/data/Classes.java
@@ -2,30 +2,46 @@ package io.github.mosps.data;
 
 public enum Classes {
 
-    EARTHFORT("ヘヴィガーディアン 剛身型"),
-    BLOCK("ヘヴィガーディアン 剛守型"),
-    SHIELD("シールドファイター 光盾型"),
-    RECOVERY("シールドファイター 光砕型"),
-    LIFEBIND("ヴァーダントオラクル 森癒型"),
-    SMITE("ヴァーダントオラクル 威咲型"),
-    DISSONANCE("ビートパフォーマー 狂音型"),
-    CONCERTO("ビートパフォーマー 響奏型"),
-    MOONSTRIKE("ストームブレイド 月影型"),
-    IAIDO_SLASH("ストームブレード 雷刃型"),
-    SKYWARD("ゲイルランサー 乱風型"),
-    VANGUARD("ゲイルランサー 烈風型"),
-    ICICLE("フロストメイジ 氷牙型"),
-    FROSTBEAM("フロストメイジ 霜天型"),
-    WILDPACK("ディバインアーチャー 狼弓型"),
-    FALCONRY("ディバインアーチャー 鷹弓型");
+    EARTHFORT("ヘヴィガーディアン", "剛身型", "<:class_1:1480204223835537419>"),
+    BLOCK("ヘヴィガーディアン", "剛守型", "<:class_1:1480204223835537419>"),
+    SHIELD("シールドファイター", "光盾型", "<:class_2:1480204339556385002>"),
+    RECOVERY("シールドファイター", "光砕型", "<:class_2:1480204339556385002>"),
+    LIFEBIND("ヴァーダントオラクル", "森癒型", "<:class_3:1480204268375113818>"),
+    SMITE("ヴァーダントオラクル", "威咲型", "<:class_3:1480204268375113818>"),
+    DISSONANCE("ビートパフォーマー", "狂音型", "<:class_4:1480204374180499477>"),
+    CONCERTO("ビートパフォーマー", "響奏型", "<:class_4:1480204374180499477>"),
+    MOONSTRIKE("ストームブレイド", "月影型", "<:class_5:1480210020477177866>"),
+    IAIDO_SLASH("ストームブレード", "雷刃型", "<:class_5:1480210020477177866>"),
+    SKYWARD("ゲイルランサー", "乱風型", "<:class_6:1480204412390608966>"),
+    VANGUARD("ゲイルランサー", "烈風型", "<:class_6:1480204412390608966>"),
+    ICICLE("フロストメイジ", "氷牙型", "<:class_7:1480204306182308042>"),
+    FROSTBEAM("フロストメイジ", "霜天型", "<:class_7:1480204306182308042>"),
+    WILDPACK("ディバインアーチャー", "狼弓型", "<:class_8:1480211556590551073>"),
+    FALCONRY("ディバインアーチャー", "鷹弓型", "<:class_8:1480211556590551073>");
 
-    private final String label;
+    private final String name;
+    private final String style;
+    private final String emoji;
 
-    Classes(String label) {
-        this.label = label;
+    Classes(String name, String style, String emoji) {
+        this.name = name;
+        this.style = style;
+        this.emoji = emoji;
     }
 
-    public String getLabel() {
-        return label;
+    public String getName() {
+        return name;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public String getEmoji() {
+        return emoji;
+    }
+
+    public String getDisplay() {
+        return emoji + name + style;
     }
 }
