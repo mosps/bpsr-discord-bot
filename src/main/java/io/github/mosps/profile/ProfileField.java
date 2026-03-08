d1d4abdd275bf55bbe6179b9b93771a72637ed90
mosps <Mos.ps2020@gmail.com>
2026-03-09 01:32:23 +0900

    ProfileField用enumを作成

--- src/main/java/io/github/mosps/profile/ProfileField.java
+++ src/main/java/io/github/mosps/profile/ProfileField.java
@@ -1,7 +1,66 @@
 package io.github.mosps.profile;
 
+import io.github.mosps.data.Classes;
+import io.github.mosps.data.Imagines;
+
 public enum ProfileField {
 
+    MAIN_CLASS("main_class") {
+        @Override
+        public void apply(Profile profile, String value) {
+            profile.setMainClass(Classes.valueOf(value));
+        }
 
+        @Override
+        public void reset(Profile profile) {
+        }
+    },
+
+    SUB_CLASS("sub_class") {
+        @Override
+        public void apply(Profile profile, String value) {
+            profile.addSubClass(Classes.valueOf(value));
+        }
+
+        @Override
+        public void reset(Profile profile) {
+            profile.resetSubClasses();
+        }
+    },
+
+    IMAGINE("imagine") {
+        @Override
+        public void apply(Profile profile, String value) {
+            profile.addImagine(Imagines.valueOf(value));
+        }
+
+        @Override
+        public void reset(Profile profile) {
+            profile.resetImagines();
+        }
+    };
+
+    private final String id;
+
+    ProfileField(String id) {
+        this.id = id;
+    }
+
+    public abstract void apply(Profile profile, String value);
+
+    public abstract void reset(Profile profile);
+
+    public String getId() {
+        return id;
+    }
+
+    public static ProfileField fromId(String id) {
+        for (ProfileField field : values()) {
+            if (field.id.equals(id)) {
+                return field;
+            }
+        }
+        return null;
+    }
 }
 
