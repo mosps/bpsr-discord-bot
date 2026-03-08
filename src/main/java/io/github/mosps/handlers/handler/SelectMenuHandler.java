6aa515e8ad4836b9015c87795beca0360ad29850
mosps <Mos.ps2020@gmail.com>
2026-03-09 02:15:54 +0900

    本人以外menuを操作できないように変更

--- src/main/java/io/github/mosps/handlers/handler/SelectMenuHandler.java
+++ src/main/java/io/github/mosps/handlers/handler/SelectMenuHandler.java
@@ -1,6 +1,5 @@
 package io.github.mosps.handlers.handler;
 
-import io.github.mosps.data.Classes;
 import io.github.mosps.handlers.actions.Action;
 import io.github.mosps.handlers.actions.ActionContext;
 import io.github.mosps.handlers.actions.ActionManager;
@@ -16,6 +15,14 @@ public class SelectMenuHandler {
         String[] customId = event.getComponentId().split(":");
         String key = customId[0] + ":" + customId[1];
         String type = customId[2];
+        String ownerId = customId[3];
+
+        if (!event.getUser().getId().equals(ownerId)) {
+            event.getHook().sendMessage("あなたはこのメニューを操作できません。")
+                    .setEphemeral(true)
+                    .queue();
+            return;
+        }
 
         ActionContext context = new ActionContext(event.getUser().getIdLong(), event.getUser().getEffectiveName(), type, event.getValues());
         Action action = ActionManager.get(key);
