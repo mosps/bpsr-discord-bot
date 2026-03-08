2ed1dbc362160f7a63ccdb7eced054cba1498cf1
mosps <Mos.ps2020@gmail.com>
2026-03-09 01:34:41 +0900

    SelectMenu用のResponderを作成

--- src/main/java/io/github/mosps/handlers/response/SelectMenuResponder.java
+++ src/main/java/io/github/mosps/handlers/response/SelectMenuResponder.java
@@ -1,4 +1,30 @@
 package io.github.mosps.handlers.response;
 
-public class SelectMenuResponder {
+import io.github.mosps.render.RenderResult;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+
+public class SelectMenuResponder implements Responder {
+
+    private final InteractionHook hook;
+
+    public SelectMenuResponder(StringSelectInteractionEvent event) {
+        this.hook = event.getHook();
+    }
+
+    @Override
+    public void update(RenderResult render) {
+        hook.editOriginal(
+                render.getMessageEditData()
+        ).setComponents(
+                render.getComponents()
+        ).queue();
+    }
+
+    @Override
+    public void ephemeral(String message) {
+        hook.sendMessage(message)
+                .setEphemeral(true)
+                .queue();
+    }
 }
