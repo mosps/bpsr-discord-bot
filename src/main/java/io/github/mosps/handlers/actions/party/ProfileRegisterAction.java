a29d823fa5d2992562c4e1153a35d87fb757681c
mosps <Mos.ps2020@gmail.com>
2026-03-09 01:33:09 +0900

    処理を実装

--- src/main/java/io/github/mosps/handlers/actions/party/ProfileRegisterAction.java
+++ src/main/java/io/github/mosps/handlers/actions/party/ProfileRegisterAction.java
@@ -1,13 +1,37 @@
 package io.github.mosps.handlers.actions.party;
 
+import io.github.mosps.data.Classes;
 import io.github.mosps.handlers.actions.Action;
 import io.github.mosps.handlers.actions.ActionContext;
 import io.github.mosps.handlers.actions.ActionResult;
+import io.github.mosps.profile.Profile;
+import io.github.mosps.profile.ProfileField;
+import io.github.mosps.profile.ProfileManager;
+import io.github.mosps.render.MessageRenderer;
+import io.github.mosps.render.RenderResult;
+import io.github.mosps.views.profile.ProfileView;
 
 public class ProfileRegisterAction implements Action {
 
     @Override
     public ActionResult execute(ActionContext context) {
-        return ActionResult.error();
+        Profile profile = ProfileManager.getProfile(context.getUserId());
+
+        profile.setName(context.getName());
+
+        ProfileField field = ProfileField.fromId(context.getType());
+        if (field == null) {
+            return ActionResult.error();
+        }
+
+        field.reset(profile);
+        context.getValue().forEach(value -> field.apply(profile, value));
+
+        ProfileManager.saveProfile(profile);
+
+        ProfileView view = ProfileManager.createView(profile);
+        RenderResult render = MessageRenderer.render(view);
+
+        return ActionResult.success(render);
     }
 }
