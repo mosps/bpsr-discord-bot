1a903c82c267f49c42d46962db0ffc22106c2995
mosps <Mos.ps2020@gmail.com>
2026-03-09 01:34:22 +0900

    SelectMenu用のListenerを作成

--- src/main/java/io/github/mosps/handlers/listener/SelectMenuListener.java
+++ src/main/java/io/github/mosps/handlers/listener/SelectMenuListener.java
@@ -1,4 +1,19 @@
 package io.github.mosps.handlers.listener;
 
-public class SelectMenuListener {
+import io.github.mosps.handlers.handler.ButtonHandler;
+import io.github.mosps.handlers.handler.SelectMenuHandler;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.event.ActionListener;
+
+public class SelectMenuListener extends ListenerAdapter {
+
+    @Override
+    public void onStringSelectInteraction(StringSelectInteractionEvent event) {
+        if (!event.isAcknowledged()) {
+            event.deferEdit().queue();
+        }
+        SelectMenuHandler.handle(event);
+    }
 }
