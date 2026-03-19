b6a73f690ee450d779289855b24be8be5fc2bf65
mosps <Mos.ps2020@gmail.com>
2026-03-19 23:44:22 +0900

    クラスを追加

---
+++ src/main/java/io/github/mosps/handlers/actions/profile/imagine/ImagineTierAction.java
@@ -0,0 +1,13 @@
+package io.github.mosps.handlers.actions.profile.imagine;
+
+import io.github.mosps.handlers.actions.Action;
+import io.github.mosps.handlers.actions.ActionContext;
+import io.github.mosps.handlers.actions.ActionResult;
+
+public class ImagineTierAction implements Action {
+
+    @Override
+    public ActionResult execute(ActionContext context) {
+
+    }
+}
