1e088a10c9dbd8f47423536a07c4a0b39829681a
mosps <Mos.ps2020@gmail.com>
2026-03-19 23:45:25 +0900

    不要なimportを削除

--- src/main/java/io/github/mosps/BPSRInviteManager.java
+++ src/main/java/io/github/mosps/BPSRInviteManager.java
@@ -7,7 +7,6 @@ import io.github.mosps.profile.ProfileManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
