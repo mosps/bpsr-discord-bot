dad31583f81a787cc605c039a618de6908932db4
mosps <Mos.ps2020@gmail.com>
2026-03-24 04:04:03 +0900

    TODO更新

--- src/main/java/io/github/mosps/BPSRInviteManager.java
+++ src/main/java/io/github/mosps/BPSRInviteManager.java
@@ -48,9 +48,12 @@ public class BPSRInviteManager {
 }
 
 /*TODO
-PartyCreateActionを編集から新規メッセージへ
-Party画面に作成時刻、期限を表示UI更新時に期限も更新.
+Party
+画面に作成時刻、期限を表示UI更新時に期限も更新.
 Party状態に応じてEmbedColorを変更
 時刻、備考はメッセージ入力によって取得..?
 目的地によって最大人数を変更可能に
+
+Profile
+ページ機能実装
 */
