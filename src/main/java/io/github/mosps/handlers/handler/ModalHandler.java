7c156d107d1c69baf06cbed1fa5f2f06848b0bcf
mosps <Mos.ps2020@gmail.com>
2026-03-27 19:39:52 +0900

    Modal用Handlerを実装

--- src/main/java/io/github/mosps/handlers/handler/ModalHandler.java
+++ src/main/java/io/github/mosps/handlers/handler/ModalHandler.java
@@ -1,4 +1,29 @@
 package io.github.mosps.handlers.handler;
 
+import io.github.mosps.handlers.actions.Action;
+import io.github.mosps.handlers.actions.ActionContext;
+import io.github.mosps.handlers.actions.ActionManager;
+import io.github.mosps.handlers.actions.ActionResult;
+import io.github.mosps.handlers.response.ButtonResponder;
+import io.github.mosps.handlers.response.ModalResponder;
+import io.github.mosps.handlers.response.Responder;
+import io.github.mosps.handlers.response.ResponseDispatcher;
+import io.github.mosps.util.customid.CustomId;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+
 public class ModalHandler {
+
+    public static void handle(ModalInteractionEvent event) {
+        CustomId customId = CustomId.of(event.getModalId());
+        long userId = event.getUser().getIdLong();
+        String name = event.getUser().getEffectiveName();
+
+        ActionContext context = new ActionContext(userId, name, customId, event.);
+        Action action = ActionManager.get(customId.getKey());
+
+        ActionResult result = action.execute(context);
+
+        Responder responder = new ModalResponder(event);
+        ResponseDispatcher.dispatch(responder, result);
+    }
 }
--- src/main/java/io/github/mosps/handlers/listener/ModalListener.java
+++ src/main/java/io/github/mosps/handlers/listener/ModalListener.java
@@ -1,4 +1,16 @@
 package io.github.mosps.handlers.listener;
 
-public class ModalListener {
+import io.github.mosps.handlers.handler.ModalHandler;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class ModalListener extends ListenerAdapter {
+
+    @Override
+    public void onModalInteraction(ModalInteractionEvent event) {
+        if (!event.isAcknowledged()) {
+            event.deferEdit().queue();
+        }
+        ModalHandler.handle(event);
+    }
 }
