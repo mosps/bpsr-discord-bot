a500129c50bca3a49993689dc3a8b34ecff74eab
mosps <Mos.ps2020@gmail.com>
2026-03-30 17:26:06 +0900

    PartyModalActionを実装

--- src/main/java/io/github/mosps/handlers/actions/party/PartyModalAction.java
+++ src/main/java/io/github/mosps/handlers/actions/party/PartyModalAction.java
@@ -1,4 +1,39 @@
 package io.github.mosps.handlers.actions.party;
 
-public class PartyModalAction {
+import io.github.mosps.handlers.actions.Action;
+import io.github.mosps.handlers.actions.ActionContext;
+import io.github.mosps.handlers.actions.ActionResult;
+import net.dv8tion.jda.api.components.label.Label;
+import net.dv8tion.jda.api.components.textinput.TextInput;
+import net.dv8tion.jda.api.components.textinput.TextInputStyle;
+import net.dv8tion.jda.api.modals.Modal;
+
+public class PartyModalAction implements Action {
+
+    @Override
+    public ActionResult execute(ActionContext context) {
+        TextInput destination = TextInput.create("destination", TextInputStyle.SHORT)
+                .setPlaceholder("目的地")
+                .setRequired(true)
+                .build();
+
+        TextInput time = TextInput.create("time", TextInputStyle.SHORT)
+                .setPlaceholder("開始時刻")
+                .setRequired(true)
+                .build();
+
+        TextInput note = TextInput.create("note", TextInputStyle.PARAGRAPH)
+                .setPlaceholder("備考")
+                .setRequired(false)
+                .build();
+
+        Modal modal = Modal.create("party:setting:" + context.getUserId(), "パーティ設定").
+                addComponents(
+                        Label.of("目的地", destination),
+                        Label.of("開始時刻", time),
+                        Label.of("備考", note)
+                ).build();
+
+        return ActionResult.of().withModal(modal);
+    }
 }
