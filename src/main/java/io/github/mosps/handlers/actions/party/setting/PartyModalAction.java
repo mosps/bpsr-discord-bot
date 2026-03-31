a8bc08259bf9723fa3a14d7d172585917b180fdd
mosps <Mos.ps2020@gmail.com>
2026-04-01 01:25:12 +0900

    クラスの階層構造を調整

--- src/main/java/io/github/mosps/handlers/actions/party/setting/PartyModalAction.java
+++ src/main/java/io/github/mosps/handlers/actions/party/setting/PartyModalAction.java
@@ -1,4 +1,4 @@
-package io.github.mosps.handlers.actions.party;
+package io.github.mosps.handlers.actions.party.setting;
 
 import io.github.mosps.handlers.actions.Action;
 import io.github.mosps.handlers.actions.ActionContext;
