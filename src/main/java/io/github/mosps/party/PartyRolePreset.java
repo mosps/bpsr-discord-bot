47146903c1e464cd9175312bf9852c0120d327ca
mosps <Mos.ps2020@gmail.com>
2026-04-03 01:52:16 +0900

    ロールプリセットを作成

--- src/main/java/io/github/mosps/party/PartyRolePreset.java
+++ src/main/java/io/github/mosps/party/PartyRolePreset.java
@@ -1,4 +1,34 @@
 package io.github.mosps.party;
 
+import io.github.mosps.data.Role;
+
+import java.util.Map;
+
 public enum PartyRolePreset {
+
+    NORMAL_5(Map.of(
+            Role.TANK, 1,
+            Role.HEALER, 1,
+            Role.DPS, 3
+    )),
+
+    FREE_5(Map.of(Role.ALL, 5)),
+
+    NORMAL_20(Map.of(
+            Role.TANK, 2,
+            Role.HEALER, 4,
+            Role.DPS, 14
+    )),
+
+    FREE_20(Map.of(Role.ALL, 20));
+
+    private final Map<Role, Integer> value;
+
+    PartyRolePreset(Map<Role, Integer> value) {
+        this.value = value;
+    }
+
+    public Map<Role, Integer> getValue() {
+        return value;
+    }
 }
