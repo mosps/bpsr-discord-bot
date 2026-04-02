fc84b4998ac9d559a97f1ccea69efa06ae2b10cb
mosps <Mos.ps2020@gmail.com>
2026-04-03 01:52:45 +0900

    ロールプリセット選択時のActionを実装

--- src/main/java/io/github/mosps/handlers/actions/party/setting/PartyRoleSettingAction.java
+++ src/main/java/io/github/mosps/handlers/actions/party/setting/PartyRoleSettingAction.java
@@ -1,4 +1,26 @@
 package io.github.mosps.handlers.actions.party.setting;
 
-public class PartyRoleSettingAction {
+import io.github.mosps.handlers.actions.Action;
+import io.github.mosps.handlers.actions.ActionContext;
+import io.github.mosps.handlers.actions.ActionResult;
+import io.github.mosps.handlers.actions.data.SelectMenuData;
+import io.github.mosps.party.Party;
+import io.github.mosps.party.PartyManager;
+import io.github.mosps.party.PartyRoleManager;
+import io.github.mosps.party.PartyRolePreset;
+
+public class PartyRoleSettingAction implements Action {
+
+    @Override
+    public ActionResult execute(ActionContext context) {
+        Party party = PartyManager.getParty(context.getCustomId().get("partyId"));
+
+        SelectMenuData data = context.getData(SelectMenuData.class);
+        String selected = data.get().getFirst();
+
+        party.setPreset(PartyRolePreset.valueOf(selected));
+
+        return ActionResult.of()
+                .withEphemeral("ロール構成を変更しました");
+    }
 }
