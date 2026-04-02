8f5f5407f56c45798f7500ef6cb86e3f9800237e
mosps <Mos.ps2020@gmail.com>
2026-04-03 01:52:06 +0900

    ロールデータを作成

--- src/main/java/io/github/mosps/data/Role.java
+++ src/main/java/io/github/mosps/data/Role.java
@@ -1,4 +1,18 @@
 package io.github.mosps.data;
 
 public enum Role {
+    TANK("T"),
+    HEALER("H"),
+    DPS("D"),
+    ALL("A");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
