ea17bb2a5988837514122189a49aa4df81b79af2
mosps <Mos.ps2020@gmail.com>
2026-04-03 22:02:56 +0900

    クラスを追加

---
+++ src/main/java/io/github/mosps/mapper/PartySettingViewMapper.java
@@ -0,0 +1,9 @@
+package io.github.mosps.mapper;
+
+import io.github.mosps.party.Party;
+import io.github.mosps.views.party.PartyView;
+import io.github.mosps.views.party.setting.PartySettingView;
+
+public class PartySettingViewMapper implements ViewMapper<Party, PartySettingView> {
+
+}
