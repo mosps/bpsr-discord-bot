10330a333fac0bde2fb5d85e5b3d4a3ac65e50fa
mosps <Mos.ps2020@gmail.com>
2026-04-05 21:41:01 +0900

    PartyStorageを追加

---
+++ src/main/java/io/github/mosps/util/PartyStorage.java
@@ -0,0 +1,75 @@
+package io.github.mosps.util;
+
+import com.google.gson.Gson;
+import io.github.mosps.model.party.Party;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+public class PartyStorage {
+
+    private static final Gson gson = new Gson();
+    private static final Path path = Paths.get("parties");
+
+    private static final Logger logger = Logger.getLogger(PartyStorage.class.getName());
+
+    public static void save(Party party) {
+        try {
+            Files.createDirectories(path);
+
+            Path file = path.resolve(party.getPartyId() + ".json");
+            String json = gson.toJson(party);
+
+            Files.writeString(file, json);
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Could not save party to file", e);
+        }
+    }
+
+    public static Party load(String partyId) {
+        try {
+            Path file = path.resolve(partyId + ".json");
+
+            if (!Files.exists(file)) return null;
+
+            String json = Files.readString(file);
+
+            return gson.fromJson(json, Party.class);
+        } catch (IOException e)  {
+            logger.log(Level.WARNING, "Could not load party from file", e);
+        }
+
+        return null;
+    }
+
+    public static Map<String, Party> loadAll() {
+        Map<String, Party> parties = new HashMap<>();
+
+        try {
+            Files.createDirectories(path);
+
+            try (Stream<Path> paths = Files.list(path)) {
+                paths.forEach(file -> {
+                    try {
+                        String json = Files.readString(file);
+                        Party party = gson.fromJson(json, Party.class);
+                        parties.put(party.getPartyId(), party);
+                    } catch (IOException e) {
+                        logger.log(Level.WARNING, "Could not load party from file", e);
+                    }
+                });
+            }
+        }  catch (IOException e) {
+            logger.log(Level.WARNING, "Could not load parties", e);
+        }
+
+        return parties;
+    }
+}
--- src/main/java/io/github/mosps/util/PartyStrorage.java
+++
@@ -1,4 +0,0 @@
-package io.github.mosps.util;
-
-public class PartyStrorage {
-}
