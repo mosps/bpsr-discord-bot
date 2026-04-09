30aec941243b6d6d1ddc16c2acaeefffa0e210ec
mosps <Mos.ps2020@gmail.com>
2026-04-10 03:16:44 +0900

    guildIdを要素追加

--- src/main/java/io/github/mosps/actions/ActionContext.java
+++ src/main/java/io/github/mosps/actions/ActionContext.java
@@ -5,18 +5,24 @@ import io.github.mosps.actions.data.EmptyData;
 import io.github.mosps.util.customid.CustomId;
 
 public class ActionContext {
+    private final long guildId;
     private final String messageId;
     private final long userId;
     private final String name;
     private final CustomId customId;
     private final ActionData data;
 
-    public ActionContext(String messageId, long userId, String name, CustomId customId, ActionData data) {
+    public ActionContext(long guildId, String messageId, long userId, String name, CustomId customId, ActionData data) {
+        this.guildId = guildId;
         this.messageId = messageId;
         this.userId = userId;
         this.name = name;
         this.customId = customId;
         this.data = data;
+    }
+
+    public long getGuildId() {
+        return guildId;
     }
 
     public String getMessageId() {
