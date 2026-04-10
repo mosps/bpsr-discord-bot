89195585b266595938a4a120ae9450b2e3b096a8
mosps <Mos.ps2020@gmail.com>
2026-04-10 20:59:17 +0900

    ギルドイベントの判定方法を変更

--- src/main/java/io/github/mosps/jda/handler/Handler.java
+++ src/main/java/io/github/mosps/jda/handler/Handler.java
@@ -2,13 +2,16 @@ package io.github.mosps.jda.handler;
 
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.guild.GenericGuildEvent;
+import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
+
+import java.util.Objects;
 
 public abstract class Handler {
 
-    protected static long requireGuildId(GenericEvent event) {
-        if (!(event instanceof GenericGuildEvent guildEvent)) {
+    protected static long requireGuildId(GenericInteractionCreateEvent event) {
+        if (!(event.isFromGuild())) {
             throw new IllegalStateException("This event is not a guild event");
         }
-        return guildEvent.getGuild().getIdLong();
+        return Objects.requireNonNull(event.getGuild()).getIdLong();
     }
 }
