44da51b621081274bf6a8c7afe5da465dbdb99ad
mosps <Mos.ps2020@gmail.com>
2026-04-10 22:15:01 +0900

    sessionに有効期限を設定 操作時に有効期限を延長するように変更 session作成専用関数を追加

--- src/main/java/io/github/mosps/model/profile/imagine/ImagineEditManager.java
+++ src/main/java/io/github/mosps/model/profile/imagine/ImagineEditManager.java
@@ -4,21 +4,44 @@ import io.github.mosps.model.profile.Profile;
 import io.github.mosps.model.profile.ProfileManager;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 
 public class ImagineEditManager {
 
     private static final Map<Long, ImagineEditSession> sessions = new ConcurrentHashMap<>();
 
-    public static ImagineEditSession get(long guildId, long userId) {
-        return sessions.computeIfAbsent(userId, id -> {
-            Profile profile = ProfileManager.getProfile(id);
+    private static final ScheduledExecutorService cleaner = Executors.newSingleThreadScheduledExecutor();
+    private static ScheduledFuture<?> cleanerTask;
 
-            return new ImagineEditSession(id, profile.getImagines());
-        });
+    private static final long TIMEOUT = TimeUnit.MINUTES.toMillis(10);
+
+    public static ImagineEditSession get(long userId) {
+        ImagineEditSession session = sessions.get(userId);
+
+        session.updateTime();
+
+        return session;
+    }
+
+    public static ImagineEditSession createSession(long userId) {
+        Profile profile = ProfileManager.getProfile(userId);
+        return new ImagineEditSession(userId, profile.getImagines());
     }
 
     public static void remove(long userId) {
         sessions.remove(userId);
+    }
+
+    public static void startCleaner() {
+        if (cleanerTask != null && !cleanerTask.isCancelled()) return;
+
+        cleanerTask = cleaner.scheduleAtFixedRate(() -> {
+            long currentTime = System.currentTimeMillis();
+
+            sessions.entrySet().removeIf(entry -> {
+                ImagineEditSession session = entry.getValue();
+                return currentTime - session.getLastTime() > TIMEOUT;
+            });
+        }, 5, 5, TimeUnit.MINUTES);
     }
 }
