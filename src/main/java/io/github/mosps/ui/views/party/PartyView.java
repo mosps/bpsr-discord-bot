a4a20b8b1063ee93987016dd1fda51e7cf9e5a1e
mosps <Mos.ps2020@gmail.com>
2026-04-10 20:26:43 +0900

    memberIdListからmemberViewListに変更

--- src/main/java/io/github/mosps/ui/views/party/PartyView.java
+++ src/main/java/io/github/mosps/ui/views/party/PartyView.java
@@ -1,14 +1,16 @@
 package io.github.mosps.ui.views.party;
 
 import io.github.mosps.model.data.Role;
+import io.github.mosps.ui.views.profile.MemberView;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class PartyView {
     public long ownerId;
     public long partyId;
-    public Set<Long> members;
+    public List<MemberView> members;
     public Map<Role, Integer> role;
     public boolean closed;
 
